a7b030c6746aa88bbe0b788d5156f5d4799d4b73
omkar1kadam <omkarakadam2408@gmail.com>
2025-07-26 11:26:07 +0530

    catalog purpose

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/test')
-def test():
-    return "✅ Flask is working!"
-
-if __name__ == "__main__":
-    print("🔥 Flask is running...")
-    app.run(debug=True)
+import tensorflow as tf
+print(tf.__version__)
